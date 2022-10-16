d8ed5aece54aaad8cec7955ef386fbec15a01c4a
bastienapp <bkrettly@simplon.co>
2022-10-16 18:13:56 +0200

    add one game

---
+++ game.js
@@ -0,0 +1,76 @@
+const game = {
+  id: 4200,
+  slug: "portal-2",
+  name: "Portal 2",
+  released: "2011-04-19",
+  background_image:
+    "https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg",
+  rating: 4.6,
+  saturated_color: "0f0f0f",
+  dominant_color: "0f0f0f",
+  genres: [
+    {
+      id: 2,
+      name: "Shooter",
+      slug: "shooter",
+      games_count: 21882,
+      image_background:
+        "https://media.rawg.io/media/games/198/1988a337305e008b41d7f536ce9b73f6.jpg",
+    },
+    {
+      id: 7,
+      name: "Puzzle",
+      slug: "puzzle",
+      games_count: 42966,
+      image_background:
+        "https://media.rawg.io/media/screenshots/aa7/aa7cc3831b020d2830a6e4cf7b40a73b.jpeg",
+    },
+  ],
+  clip: {
+    clip: "https://media.rawg.io/media/stories-640/fde/fde8aaeeab956f6b705bbb4161b09004.mp4",
+    clips: {
+      320: "https://media.rawg.io/media/stories-320/b26/b265f65b9f16dc20245863636d4094b2.mp4",
+      640: "https://media.rawg.io/media/stories-640/fde/fde8aaeeab956f6b705bbb4161b09004.mp4",
+      full: "https://media.rawg.io/media/stories/671/67196dea179367b70212bdaed88ba451.mp4",
+    },
+    preview:
+      "https://media.rawg.io/media/stories-previews/faf/faf0bb37b806db65f1c76395c8f36c7c.jpg",
+  },
+  short_screenshots: [
+    {
+      id: -1,
+      image:
+        "https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg",
+    },
+    {
+      id: 99018,
+      image:
+        "https://media.rawg.io/media/screenshots/221/221a03c11e5ff9f765d62f60d4b4cbf5.jpg",
+    },
+    {
+      id: 99019,
+      image:
+        "https://media.rawg.io/media/screenshots/173/1737ff43c14f40294011a209b1012875.jpg",
+    },
+    {
+      id: 99020,
+      image:
+        "https://media.rawg.io/media/screenshots/b11/b11a2ae0664f0e8a1ef2346f99df26e1.jpg",
+    },
+    {
+      id: 99021,
+      image:
+        "https://media.rawg.io/media/screenshots/9b1/9b107a790909b31918ebe2f40547cc85.jpg",
+    },
+    {
+      id: 99022,
+      image:
+        "https://media.rawg.io/media/screenshots/d05/d058fc7f7fa6128916c311eb14267fed.jpg",
+    },
+    {
+      id: 99023,
+      image:
+        "https://media.rawg.io/media/screenshots/415/41543dcc12dffc8e97d85a56ad42cda8.jpg",
+    },
+  ],
+};
