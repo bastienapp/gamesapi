11df35e91f70854a4ee161d5f8340defa10d9872
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-06-28 15:34:16 +0200

    add game list

---
+++ gameList.js
@@ -0,0 +1,97 @@
+const gameList = [
+  {
+    "id": 4200,
+    "name": "Portal 2",
+    "background_image": "https://media.rawg.io/media/games/328/3283617cb7d75d67257fc58339188742.jpg",
+  },
+  {
+    "id": 3498,
+    "name": "Grand Theft Auto V",
+    "background_image": "https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg",
+  },
+  {
+    "id": 3328,
+    "name": "The Witcher 3: Wild Hunt",
+    "background_image": "https://media.rawg.io/media/games/088/088b41ca3f9d22163e43be07acf42304.jpg",
+  },
+  {
+    "id": 12020,
+    "name": "Left 4 Dead 2",
+    "background_image": "https://media.rawg.io/media/games/c25/c25ebb8eb08790277ae2e2dce0d62174.jpg",
+  },
+  {
+    "id": 5679,
+    "name": "The Elder Scrolls V: Skyrim",
+    "background_image": "https://media.rawg.io/media/games/148/1485f72434101885b098d4290bf0ba67.jpg",
+  },
+  {
+    "id": 802,
+    "name": "Borderlands 2",
+    "background_image": "https://media.rawg.io/media/games/588/588c6bdff3d4baf66ec36b1c05b793bf.jpg",
+  },
+  {
+    "id": 3439,
+    "name": "Life is Strange",
+    "background_image": "https://media.rawg.io/media/games/562/562553814dd54e001a541e4ee83a591c.jpg",
+  },
+  {
+    "id": 13536,
+    "name": "Portal",
+    "background_image": "https://media.rawg.io/media/screenshots/19d/19d3effb85e8f40d0b5b004fb5ab5c76.jpg",
+  },
+  {
+    "id": 4291,
+    "name": "Counter-Strike: Global Offensive",
+    "background_image": "https://media.rawg.io/media/games/ded/dedd631793c5666ffab9ef03975eb226.jpg",
+  },
+  {
+    "id": 4062,
+    "name": "BioShock Infinite",
+    "background_image": "https://media.rawg.io/media/games/fc1/fc1307a2774506b5bd65d7e8424664a7.jpg",
+  },
+  {
+    "id": 1030,
+    "name": "Limbo",
+    "background_image": "https://media.rawg.io/media/games/929/9295e55ce69cf5337c567983cf8b4137.jpeg",
+  },
+  {
+    "id": 5286,
+    "name": "Tomb Raider (2013)",
+    "background_image": "https://media.rawg.io/media/games/81b/81b138691f027ed1f8720758daa0d895.jpg",
+  },
+  {
+    "id": 11859,
+    "name": "Team Fortress 2",
+    "background_image": "https://media.rawg.io/media/games/8e0/8e032ac4faf1136e7d708bb3ac61af23.jpg",
+  },
+  {
+    "id": 13537,
+    "name": "Half-Life 2",
+    "background_image": "https://media.rawg.io/media/games/b8c/b8c243eaa0fbac8115e0cdccac3f91dc.jpg",
+  },
+  {
+    "id": 4286,
+    "name": "BioShock",
+    "background_image": "https://media.rawg.io/media/games/bc0/bc06a29ceac58652b684deefe7d56099.jpg",
+  },
+  {
+    "id": 3939,
+    "name": "PAYDAY 2",
+    "background_image": "https://media.rawg.io/media/games/73e/73eecb8909e0c39fb246f457b5d6cbbe.jpg",
+  },
+  {
+    "id": 4459,
+    "name": "Grand Theft Auto IV",
+    "background_image": "https://media.rawg.io/media/games/4a0/4a0a1316102366260e6f38fd2a9cfdce.jpg",
+  },
+  {
+    "id": 3070,
+    "name": "Fallout 4",
+    "background_image": "https://media.rawg.io/media/games/c88/c885ae6a24bbce21df93b6c4e1c78899.jpg",
+  },
+  {
+    "id": 766,
+    "name": "Warframe",
+    "background_image": "https://media.rawg.io/media/games/2c4/2c4ec7b64079b561667850593d23c417.jpg",
+  }
+]
